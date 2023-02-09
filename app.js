8a8ebd01314afb6d00625297d6cd81c2e5fd275e
Muhammad Atta ul Mustafa <120771673+Atta1380@users.noreply.github.com>
2023-02-09 20:47:43 +0500

    added javascript and html file

---
+++ app.js
@@ -0,0 +1,33 @@
+//alert("atta");
+
+
+//Assignment---9-11--
+// ---question--1
+
+//var city = "karachi";
+//var city =prompt("Enter the name of city: ");
+//if(city === "karachi ") {
+  // alert("Welcom to the city of light);"
+//} else {
+//
+  //  alert("Welcome to karachi")
+//}
+
+// ---question--2
+
+//var gender =prompt("Enter your gender  (male/female); ")
+//if (gender ==="male") {
+  //  alert("Good Morning Sir");
+//} else if (gender === "female") {
+  //  alert("Good Morning Ma'am")
+//} else {
+  //  alert("Write....... male/female")
+    
+//}
+
+
+// ---question--3
+
+
+
+
